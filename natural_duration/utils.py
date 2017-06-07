955477a486233bf813f58c16b633ce01d272a4d5
Andy Grabow <andy@freilandkiwis.de>
2017-06-07 17:14:36 +0200

    damn 80 chars stuff

--- natural_duration/utils.py
+++ natural_duration/utils.py
@@ -8,15 +8,24 @@ def human_duration_string(value):
     us = abs(value.microseconds)
     seconds = abs(value.seconds)
     days = abs(value.days)
-    builder.append((_('a year'), '%d ' + str(_('years')), days // 365))
-    builder.append((_('a month'), '%d ' + str(_('months')), days % 365 // 30))
-    builder.append((_('a week'), '%d ' + str(_('weeks')), days % 365 % 30 // 7))
-    builder.append((_('a day'), '%d ' + str(_('days')), days % 365 % 30 % 7))
-    builder.append((_('an hour'), '%d ' + str(_('hours')), seconds // 60 // 60))
-    builder.append((_('a minute'), '%d ' + str(_('minutes')), seconds // 60 % 60))
-    builder.append((_('a second'), '%d ' + str(_('seconds')), seconds % 60))
-    builder.append((_('a millisecond'), '%d ' + str(_('milliseconds')), us // 1000))
-    builder.append((_('a microsecond'), '%d ' + str(_('microseconds')), us % 1000))
+    builder.append((_('a year'),
+                    '%d ' + str(_('years')), days // 365))
+    builder.append((_('a month'),
+                    '%d ' + str(_('months')), days % 365 // 30))
+    builder.append((_('a week'),
+                    '%d ' + str(_('weeks')), days % 365 % 30 // 7))
+    builder.append((_('a day'),
+                    '%d ' + str(_('days')), days % 365 % 30 % 7))
+    builder.append((_('an hour'),
+                    '%d ' + str(_('hours')), seconds // 60 // 60))
+    builder.append((_('a minute'),
+                    '%d ' + str(_('minutes')), seconds // 60 % 60))
+    builder.append((_('a second'),
+                    '%d ' + str(_('seconds')), seconds % 60))
+    builder.append((_('a millisecond'),
+                    '%d ' + str(_('milliseconds')), us // 1000))
+    builder.append((_('a microsecond'),
+                    '%d ' + str(_('microseconds')), us % 1000))
 
     legit = []
     for tup in builder:
